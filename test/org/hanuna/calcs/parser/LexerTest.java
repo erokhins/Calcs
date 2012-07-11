75659f43843f5da71d990434ef75f5dbd60d60bd
erokhins <smevok@yandex.ru>
2012-07-12 00:15:51 +0400

    Fix prev commit

--- test/org/hanuna/calcs/parser/LexerTest.java
+++ test/org/hanuna/calcs/parser/LexerTest.java
@@ -2,17 +2,40 @@ package org.hanuna.calcs.parser;
 
 import org.junit.Test;
 
+import java.io.Reader;
+import java.io.StringReader;
+
+import static junit.framework.Assert.assertTrue;
+
 /**
  * @author erokhins
  */
 public class LexerTest {
-    @Test
-    public void testNext() throws Exception {
 
+   public String lexerStringReturn(Reader r) {
+        String s = "";
+        Lexer l = new Lexer(r);
+        LexerToken t = l.next();
+        while (t.getT() >= 0) {
+            s += t.getT() + ":" + t.getS() + " ";
+            t = l.next();
+        }
+        s += t.getT();
+        return s;
     }
 
     @Test
-    public void testGetToken() throws Exception {
+    public void test1(){
+       StringReader r = new StringReader("  2 \n dd _dfg _25");
+       assertTrue("Чё за хрень???", lexerStringReturn(r).equals("11:2 12:dd 12:_dfg 12:_2 -1"));
+
+        r = new StringReader("  2 \n dd _dfg _1");
+        assertTrue("Lexer", lexerStringReturn(r).equals("11:2 12:dd 12:_dfg 12:_2 -1"));
 
     }
+
+    public static void main(String args[]) {
+        org.junit.runner.JUnitCore.main("junitfaq.SimpleTest");
+    }
+
 }
